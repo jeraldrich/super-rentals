be57f4b1e77ae8f0c9e02418716cb8dfd660bcfd
Jeremy Aldrich <jeremy@jeremyaldrich.net>
2022-06-17 00:42:21 -0400

    Add share on twitter as an example service injection with service test mock

--- tests/integration/components/rental/detailed-test.js
+++ tests/integration/components/rental/detailed-test.js
@@ -29,7 +29,8 @@ module('Integration | Component | rental/detailed', function (hooks) {
   });
 
   test('it renders a header with a share button', async function (assert) {
-    await render(hbs`<Rental::Detailed @rental={{this.rental}} />`); assert.dom('.jumbo').exists();
+    await render(hbs`<Rental::Detailed @rental={{this.rental}} />`);
+    assert.dom('.jumbo').exists();
 
     assert.dom('.jumbo h2').containsText('Grand Old Mansion');
     assert
@@ -50,6 +51,4 @@ module('Integration | Component | rental/detailed', function (hooks) {
     assert.dom('article .image').exists();
     assert.dom('article .map').exists();
   });
-
-  
 });
